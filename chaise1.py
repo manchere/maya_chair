708a5ac405df57eb18a9bebb1a4b80a25c1712cc
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-10-05 03:16:36 +0200

    chaise update

--- chaise1.py
+++ chaise1.py
@@ -1,33 +1,22 @@
 import maya.cmds as cmds
 
-cmds.polyCube(name ='leg1')
-cmds.scale(1, 5, 1, 'leg1')
-cmds.move(-2, 3, 1, 'leg1')
-
-cmds.polyCube(name ='leg2')
-cmds.scale(1, 5, 1, 'leg2')
-cmds.move(2, 3, 1, 'leg2')
-
-cmds.polyCube(name ='leg3')
-cmds.scale(1, 5, 1, 'leg3') 
-cmds.move(0, 3, 5, 'leg3')
-
-cmds.polyCylinder(name='table1')
-cmds.scale(3.154, 0.556, 3.43, 'table1')
-cmds.move(0, 5.137, 2.432, 'table1')
-
-cmds.polyCube(name ='back')
-cmds.scale(5.963, 6.334, 1, 'back')
-cmds.move(0, 8.439,1,'back')
-cmds.rotate(-10,0,0)
-
-cmds.polyCube(name ='back2')
-cmds.scale(4.552, 4.835, 0.763, 'back2')
-cmds.move(0, 8.439,1.5,'back2')
-cmds.rotate(-10,0,0)
-
-#cmds.select(back2.f[0])
-
+def legs():
+    for leg_num in range(2):
+        if leg_num % 2 == 0:
+            cmds.polyCylinder(n= 'leg0%s' % leg_num, sx=20, sy=10, h=4.5, r=.15)
+            cmds.select('leg0%s.f[100:119]' % leg_num)
+            #cmds.scale(1.857983, 1.857983, 1.857983)
+            cmds.move(-1, 3 , 1, 'leg0%s' % leg_num)
+            cmds.polyExtrudeFacet( 'leg0%s.f[0:19]' % leg_num, kft=True, ltz=.08)
+            cmds.polyBevel( 'leg0%s.f[0:19]' % leg_num, offset=0.035 )
+            cmds.select('leg0%s.f[160:179]' % leg_num)
+            cmds.scale(0.37,1,1)
+            cmds.polyTorus(n = 'knot%s' % leg_num, sr =0.4132, sx=20, sy=20, cuv=1, ch=1)
+            cmds.scale(0.062, 0.062, 0.062, 'knot%s' % leg_num)
+            cmds.rotate(0,0,90,'knot%s' % leg_num)
+            cmds.move(-1.045, 5.13, 1.002,'knot%s' % leg_num)
+            cmds.polyUnite('knot%s' % leg_num, 'leg0%s' % leg_num, n='n_leg0%s' % leg_num)
+legs()
 
     
     
