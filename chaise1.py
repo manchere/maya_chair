cc8ae60170606a436b64790364eec7a56d25c220
Manu Cheremeh <manucheremeh1995@gmail.com>
2021-10-17 03:39:59 +0200

    refactoring chair

--- chaise1.py
+++ chaise1.py
@@ -1,5 +1,6 @@
 import maya.cmds as cmds
-cmds.file(f = True, n = True)
+import math
+# On ne résout pas un grand problème avec une grosse solution, mais par une armée de petites solutions
 
 cmds.file(new=True, force=True)
 
@@ -7,18 +8,27 @@ cmds.file(new=True, force=True)
 def legs(x, y, z, rotateX, rotateY, rotateZ, leg_name):
     cmds.polyCylinder(n='leg%s' % leg_name, sx=20, sy=10, h=4.5, r=.15)
     cmds.select('leg%s.f[100:119]' % leg_name)
-    cmds.move(0, 3, 0, 'leg%s' % leg_name)
+    # cmds.move(0, 3, 0, 'leg%s' % leg_name) # unnnecessary for now
     cmds.polyExtrudeFacet( 'leg%s.f[0:19]' % leg_name, kft=True, ltz=.08)
     cmds.polyBevel('leg%s.f[0:19]' % leg_name, offset=0.035)
     cmds.select('leg%s.f[160:179]' % leg_name)
     cmds.scale(0.37,1,1)
-    cmds.polyTorus(n = 'knot%s' % leg_name, sr=0.4132, sx=20, sy=20, cuv=1, ch=1)
-    cmds.scale(0.162, 0.162, 0.162, 'knot%s' % leg_name)
-    cmds.rotate(0,0,90,'knot%s' % leg_name)
-    cmds.move(0, 5.13, 0,'knot%s' % leg_name)
-    cmds.polyUnite('knot%s' % leg_name, 'leg%s' % leg_name, n='n_leg%s' % leg_name)
+    cmds.polyUnite(chair_ring(leg_name), 'leg%s' % leg_name, n='n_leg%s' % leg_name)
+    leg_position(x, y, z, rotateX, rotateY, rotateZ)
+
+# positionement d'un pied
+def leg_position(x, y, z, rotateX, rotateY, rotateZ):
     cmds.move(x, y, z)
     cmds.rotate(rotateX, rotateY, rotateZ)
+
+# definition d'un anneau pour un pied
+def chair_ring(leg_name):
+    cmds.polyTorus(n = 'knot%s' % leg_name, sr=0.4132, sx=20, sy=20, ch=1)
+    cmds.scale(0.162, 0.162, 0.162, 'knot%s' % leg_name)
+    cmds.rotate(0,0,90,'knot%s' % leg_name)
+    cmds.move(0, 2.138, 0,'knot%s' % leg_name)
+
+    return 'knot %s' % leg_name
 
 # definition du siege de la chaise
 def seat(x, y, z, seat, rotation):
@@ -41,37 +51,61 @@ def supprimer_histoire():
     cmds.select(all = True)
     cmds.delete(constructionHistory = True)
 
-def ajuster_pivot():
-    cmds.move(0, 0, 0,  '.scalePivot', '.rotatePivot', absolute = True)
+# def ajuster_pivot():
+#     cmds.move(0, 0, 0,  '.scalePivot', '.rotatePivot', absolute = True)
 
-def etirer(name, longueur, hauteur):
-    cmds.select(name)
+def get_leg_rotate(leg_name):
+    angle = cmds.polyCylinder('leg%s' % leg_name, query = True, r = True)
+
+# obtenir le hauteur des pieds
+def get_leg_height(leg_name):
+    hypo = cmds.polyCylinder('leg%s' % leg_name, query = True, h = True)
+    return 
+
+
+def stretch(grpname, longueur, hauteur):
+    cmds.select(grpname)
     cmds.scale(longueur, hauteur, xy = True)
+    math.sin()
+    cmds.move(0,h1 *2/2+hd+hd1/2,0)
 
 
 # Creation des pieds de la chaise
-legs(-0.25, -0.514, 3.681, -150.546, -0.155, 179.288, 'arriere_droit')
-legs(1.843, -0.514, 3.681, -29.455, 0.522, -0.919, 'arriere_gauche')
-legs(-0.004, -0.514, 0.491, 149.976, -9.602, 184.291, 'devant_droit')
-legs(1.503, -0.514, 0.491, 149.976, -9.602, 184.291, 'devant_gauche')
-seat(0.871, 4.142, 1.707, 'siege', 0) # siege de la chaise
-seat(0.871, 6.304, 3.155,'dossier',-84.776) # dossier de la chaise
-barreau(0.826, 1.102, 2.771, 1.05,'b_arriere') # barreau arriere
-barreau(0.826, 1.102, 1.415, 0.76, 'b_devant') # barreau de devant
-barreau(0.826, 3.968, 1.161, 1.05, 'sous_siege') # barreau sous le siege
-barreau(0.224, 4.846, 3.136, 0.81, 'b_droit', 5.338, -0.146, 3.999) # barreau droit du dossier
-barreau(1.436, 4.846, 3.136, 0.81, 'b_gauche',5.338, -0.146, -3.999) # barreau gauche du dossier
+legs(-1.039, 0, 0, -30, 0, 0, 'arriere_droit')
+legs(1.039, 0, 0, -30, 0, 0, 'arriere_gauche')
+legs(0.75, 0, -0.43, 30, 0, 0, 'devant_droit')
+legs(-0.75, 0, -0.43, 30, 0, 0, 'devant_gauche')
 
-#supprimer l'histoire
+seat(0, 2.075, -0.38, 'siege', 0) # siege de la chaise
+seat(0, 4.15, 1,'dossier',-84.776) # dossier de la chaise
+barreau(0, -0.92, 0.536, 1.05,'b_arriere') # barreau arriere
+barreau(0, -0.92, -0.967, 0.76, 'b_devant') # barreau de devant
+barreau(0, 1.851, 0.631, 0.7, 'sous_siege') # barreau sous le siege
+barreau(-0.6, 2.5, 1, 0.81, 'b_droit', 5, 0, 4) # barreau droit du dossier
+barreau(0.6, 2.5, 1, 0.81, 'b_gauche', 5, 0, -4)  # barreau gauche du dossier
+
+# #supprimer l'histoire
 supprimer_histoire()
 
 # grouper les parties de la chaise
-cmds.group('n_legarriere_droit', 'n_legarriere_gauche', 'b_arriere',  'sous_siege', n = 'front_legs')
-cmds.group('n_legdevant_droit', 'n_legdevant_gauche', 'b_devant',n = 'back_legs')
-cmds.group('b_droit', 'b_gauche', 'fulldossier',n = 'back')
-cmds.group('back_legs', 'front_legs', n = 'full_legs')
-cmds.group('full_legs', 'back','fullsiege', n = 'chair')
-cmds.move(0, 0, 0, 'chair')
-ajuster_pivot()
 
-etirer('full_legs', 1, 5)
+# groupe pied arriere de la chaise
+cmds.group('n_legarriere_droit', 'n_legarriere_gauche', 'b_arriere',  'sous_siege', n = 'front_legs')
+
+# groupe pied devant de la chaise
+cmds.group('n_legdevant_droit', 'n_legdevant_gauche', 'b_devant',n = 'back_legs')
+
+# groupe dossier/siege de la chaise
+cmds.group('b_droit', 'b_gauche', 'fulldossier', 'fullsiege', n = 'back')
+
+# groupe tous les pieds de la chaise
+cmds.group('back_legs', 'front_legs', n = 'full_legs')
+
+# groupe toute la chaise
+cmds.group('full_legs', 'back', n = 'chair')
+
+
+# cmds.move(0, 0, 0, 'chair')
+# ajuster_pivot()
+
+stretch('full_legs', 1, 5)
